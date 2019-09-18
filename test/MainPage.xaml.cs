bea54f82db01c87cd383607a95fb3b645963eceb
Your Name <hasss95@hotmail.com>
2019-09-18 13:46:25 +0200

    init

---
+++ test/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace test
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            BatteryStats();
+        }
+
+
+        public void BatteryStats()
+        {
+            var level = Battery.ChargeLevel; // returns 0.0 to 1.0 or 1.0 when on AC or no battery.
+
+            var state = Battery.State;
+
+            switch (state)
+            {
+                case BatteryState.Charging:
+                    // Currently charging
+                    break;
+                case BatteryState.Full:
+                    // Battery is full
+                    break;
+                case BatteryState.Discharging:
+                case BatteryState.NotCharging:
+                    // Currently discharging battery or not being charged
+                    break;
+                case BatteryState.NotPresent:
+                // Battery doesn't exist in device (desktop computer)
+                case BatteryState.Unknown:
+                    // Unable to detect battery state
+                    break;
+            }
+
+            var source = Battery.PowerSource;
+
+            switch (source)
+            {
+                case BatteryPowerSource.Battery:
+                    // Being powered by the battery
+                    break;
+                case BatteryPowerSource.AC:
+                    // Being powered by A/C unit
+                    break;
+                case BatteryPowerSource.Usb:
+                    // Being powered by USB cable
+                    break;
+                case BatteryPowerSource.Wireless:
+                    // Powered via wireless charging
+                    break;
+                case BatteryPowerSource.Unknown:
+                    // Unable to detect power source
+                    break;
+            }
+
+            LabelLevel.Text = "Level: " + level + "";
+            LabelState.Text = "State: " + state + "";
+            LabelPowersource.Text = "PowerSource: " + source + "";
+        }
+       
+
+        private void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            BatteryStats();
+        }
+    }
+}
